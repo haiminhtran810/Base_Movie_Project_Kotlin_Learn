583dd634466270cadd901756af9316e1f57a67af
haiminhtran810 <haiminhtran810@gmail.com>
2020-10-12 17:33:12 +0700

    Task: movie

--- app/src/main/java/learn/htm/projectlearn/ui/detail/MovieDetailFragment.kt
+++ app/src/main/java/learn/htm/projectlearn/ui/detail/MovieDetailFragment.kt
@@ -7,11 +7,13 @@ import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.PlaybackPreparer
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ui.StyledPlayerControlView
+import learn.htm.projectlearn.BuildConfig
 import learn.htm.projectlearn.R
 import learn.htm.projectlearn.base.BaseFragment
 import learn.htm.projectlearn.binding.setOnSingleClickListener
 import learn.htm.projectlearn.databinding.FragmentMovieDetailBinding
 import learn.htm.projectlearn.model.Movie
+import learn.htm.projectlearn.model.Videos
 import learn.htm.projectlearn.ui.ShareViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -46,7 +48,6 @@ class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding, MovieDetail
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initExoPlayer()
         viewBinding.apply {
             imageViewBack.setOnSingleClickListener(View.OnClickListener {
                 requireActivity().onBackPressed()
@@ -54,14 +55,15 @@ class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding, MovieDetail
         }
     }
 
-    private fun initExoPlayer() {
+    private fun initExoPlayer(videos: Videos) {
         //Creating the player
         player = SimpleExoPlayer.Builder(requireContext()).build()
         viewBinding.apply {
             styledPlayerView.setControllerVisibilityListener(this@MovieDetailFragment)
             styledPlayerView.player = player
+            val videoUrl = BuildConfig.VIDEO_URL + videos.results.getOrNull(0)?.key
             val mediaItem =
-                MediaItem.fromUri("https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8")
+                MediaItem.fromUri(videoUrl)
             player?.setMediaItem(mediaItem)
             player?.prepare()
             player?.play()
@@ -100,6 +102,9 @@ class MovieDetailFragment : BaseFragment<FragmentMovieDetailBinding, MovieDetail
             insertSuccess.observe(viewLifecycleOwner, Observer {
                 shareViewModel.refreshMovieFavorite.call()
                 requireActivity().onBackPressed()
+            })
+            movieVideos.observe(viewLifecycleOwner, Observer {
+                initExoPlayer(it)
             })
         }
     }
